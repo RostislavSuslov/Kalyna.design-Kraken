b06f30b3b96a3faae44977c1686d1af4c6716769
RostSuslov <suslovros@gmail.com>
2023-09-28 01:07:44 +0300

    feat: creat WrapperCustomInput component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup>
+  import CustomInput from "@/components/CustomInput.vue";
+
+
 
 </script>
 
 <template>
-   <div id="root">
-     <h1 class="text-9xl">Hello Vue</h1>
+   <div id="root" class="container ">
+     <br>
+     <CustomInput/>
    </div>
 </template>
 
 <style lang="scss">
-@import './assets/css/style.scss'
+@import './assets/css/style.scss';
 
 </style>
---
+++ src/components/WrapperCustomInput.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "WrapperCustomInput"
+}
+</script>
+
+<style scoped>
+
+</style>
