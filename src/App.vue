9f03a40ab1e677c6176571a7762ae4444f7bac4c
RostSuslov <suslovros@gmail.com>
2023-09-28 03:01:41 +0300

    feat: didn't finish input

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
   import CustomInput from "@/components/CustomInput.vue";
+  import {computed, ref} from "vue"
 
+  const input = ref('')
+  const inputError = computed(()=>{
+      return input.value.trim().length ? "" : "Input is required"
+  })
 
+  const helperText = computed(()=>{
+    return "Please enter your full name in this text box.!"
+  })
 
 </script>
 
 <template>
    <div id="root" class="container ">
      <br>
-     <CustomInput/>
+     <custom-input :type="number" label="Input title" :error="inputError" :helperText="helperText"/>
    </div>
 </template>
 
--- src/components/WrapperCustomInput.vue
+++ src/components/WrapperCustomInput.vue
@@ -1,13 +1,31 @@
 <template>
+    <div class="input-wrapper max-w-screen-md mx-auto border-2 p-4">
+          <label class="text-cyan-700" :class="{'text-red-600' : hasError }" v-if="label || $slots.label">
+            <slot name="label"> {{label}} </slot>
+          </label>
+          <slot  :type="number" :_class="{'input-error' : hasError}"></slot>
+          <label class="text-red-600" v-if="hasError">{{error}}</label>
+          <slot name="helperText"><p>{{helperText}}</p></slot>
+    </div>
 
 </template>
 
-<script>
-export default {
-  name: "WrapperCustomInput"
-}
+<script setup>
+
+import {computed} from "vue";
+
+const props = defineProps({
+  label: String,
+  error: String,
+  helperText: String,
+  type: String,
+})
+const slots = defineSlots();
+
+const hasError = computed(()=> !!(props.error || slots.error))
+const hasHelperText = computed(()=> !!(props.helperText || slots.helperText))
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
