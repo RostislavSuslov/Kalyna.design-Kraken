ccc4ec3fa026628e4bb64351ea2e1a0d560656d0
RostSuslov <suslovros@gmail.com>
2023-09-28 03:01:41 +0300

    feat: Didn't finish input

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -1,19 +1,50 @@
 <template>
-  <div class="relative mb-3" data-te-input-wrapper-init>
-    <input
-      type="text"
-      class="peer block min-h-[auto] w-full rounded   ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-      id="exampleFormControlInput1"
-      placeholder="Example label"
-    />
-    <label
-      for="exampleFormControlInput1"
-      class="pointer-events-none bg-white absolute left-3 top-0 mb-0  origin-[0_0] truncate p-[0.37rem] leading-[1] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.8rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.8rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-      >Example label
-    </label>
-  </div>
+  <wrapper-custom-input :helperText="helperText" :error="error" :label="label">
+    <input :type="type" :placeholder="placeholder" class="peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0">
+  </wrapper-custom-input>
 </template>
 
-<script setup></script>
+<script setup>
+import WrapperCustomInput from "@/components/WrapperCustomInput.vue";
+
+const props = defineProps({
+  modelValue: null,
+  placeholder: String,
+  label: String,
+  error: String,
+  helperText: String,
+  type: String,
+})
+
+
+
+</script>
 
 <style scoped></style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<div class="relative mb-3" data-te-input-wrapper-init>-->
+<!--<input-->
+<!--    type="text"-->
+<!--    class="peer block min-h-[auto] w-full rounded   ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"-->
+<!--    id="exampleFormControlInput1"-->
+<!--    placeholder="Example label"-->
+<!--/>-->
+<!--<label-->
+<!--    for="exampleFormControlInput1"-->
+<!--    class="pointer-events-none bg-white absolute left-3 top-0 mb-0  origin-[0_0] truncate p-[0.37rem] leading-[1] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.8rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.8rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"-->
+<!--&gt;Example label-->
+<!--</label>-->
+<!--</div>-->
