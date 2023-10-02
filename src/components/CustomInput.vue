3ff6b6277800efa6f1e95b84b4af710842d07ded
RostSuslov <suslovros@gmail.com>
2023-10-02 10:41:52 +0300

    feat: creat form login

--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -1,22 +1,33 @@
 <template>
   <wrapper-custom-input :helperText="helperText" :error="error" :label="label">
-    <input :type="type" :placeholder="placeholder" class="peer block min-h-[auto] w-full rounded   ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0">
+    <input :type="type"
+           :placeholder="placeholder"
+           class="peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+           @input="$emit('update:modelValue', $event.target.value)"
+           :value="modelValue"
+    >
   </wrapper-custom-input>
 </template>
 
 <script setup>
 import WrapperCustomInput from "@/components/WrapperCustomInput.vue";
 
-const props = defineProps({
-  placeholder: String,
-  label: String,
-  error: String,
-  helperText: String,
-  type: String,
-})
+defineEmits(
+    ['update:modelValue', ]
+)
 
-
-
+  defineProps({
+    placeholder: String,
+    label: String,
+    error: String,
+    helperText: String,
+    modelValue: String,
+    type:  {
+      type: String,
+      default: "text"
+    },
+    name: String,
+  })
 </script>
 
 <style scoped lang="scss"></style>
---
+++ src/components/FormLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="onSubmit" >
+    <custom-input name="login" v-model="form.login"></custom-input>
+    <custom-input type="password" name="password" v-model="form.password"></custom-input>
+    <custom-input type="password" name="password" :modelValue="form.password" @update:modelValue="form.password = $event"></custom-input>
+    <button>Submit</button>
+  </form>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import CustomInput from "@/components/CustomInput.vue";
+
+const form = ref({
+  login: "Rost",
+  password: ""
+});
+const onSubmit =()=> {
+  console.log(form.value)
+}
+
+</script>
+<style scoped>
+
+</style>
