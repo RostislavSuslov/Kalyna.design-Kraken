25ce416181834fe4c8d7790f4d41f41660821380
RostSuslov <suslovros@gmail.com>
2023-10-02 09:08:55 +0300

    feat: colors provider inject done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+  import TestContainer from "@/components/TestContainer.vue";
   import CustomInput from "@/components/CustomInput.vue";
-  import {computed, ref} from "vue"
+  import { provide } from 'vue';
+  import {computed, ref} from "vue";
 
   const input = ref('')
   const inputError = computed(()=>{
@@ -9,19 +11,32 @@
 
   const helperText = computed(()=>{
     return "A smile always expresses positive emotions: happiness, joy, fun. Smile at someone and they'll smile back :)"
-  })
-
+  });
+  const arrColors = [
+    { red: '#FF5733' },
+    { green: '#33FF57' },
+    { blue: '#FFFF33' },
+    { yellow: '#33FFFF' },
+    { black: '#000' }
+  ];
+  provide('colors', arrColors);
 </script>
 
 <template>
    <div id="root" class="container ">
      <br>
      <custom-input type="text" label="Input title" :error="inputError" :helperText="helperText"/>
-
+     <div>
+       <TestContainer :arrColors="arrColors" />
+       <TestContainer :arrColors="arrColors" />
+       <TestContainer :arrColors="arrColors" />
+       <TestContainer :arrColors="arrColors" />
+     </div>
    </div>
 </template>
 
 <style lang="scss">
 @import './assets/css/style.scss';
+
 
 </style>
