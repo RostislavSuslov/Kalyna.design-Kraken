debfc4bac6decee9d3c42372e444a847ac04d586
RostSuslov <suslovros@gmail.com>
2023-10-09 14:55:17 +0300

    build: install vee-validate

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <script setup>
   import { provide } from 'vue';
-  import {computed, ref} from "vue";
-  import CustomInput from "@/components/CustomInput.vue";
-  import FormLogin from "@/components/FormLogin.vue";
-  import RegionContainer from "@/components/RegionContainer.vue";
   import arrCities from "@/composables/cities.js"
-
-  const input = ref('')
-  const inputError = computed(()=>{
-      return input.value.trim().length ? '' : 'Input is required'
-  })
-
-  const helperText = computed(()=>{
-    return "A smile always expresses positive emotions: happiness, joy, fun. Smile at someone and they'll smile back :)"
-  });
+  import {Form, Field} from 'vee-validate';
+  // const input = ref('')
+  // const inputError = computed(()=>{
+  //     return input.value.trim().length ? '' : 'Input is required'
+  // })
+  //
+  // const helperText = computed(()=>{
+  //   return "A smile always expresses positive emotions: happiness, joy, fun. Smile at someone and they'll smile back :)"
+  // });
 
   provide('cities', arrCities);
+
 </script>
 
 <template>
    <div id="root" class="container ">
-     <form-login></form-login>
-     <br>
-     <custom-input type="text" label="Input title" :error="inputError" :helperText="helperText"/>
-     <div class="grid gap-8">
-       <RegionContainer />
-       <RegionContainer />
-     </div>
+
+     <Form @submit="sendForm" class="flex flex-col items-center border p-12">
+       <h3 class="text-4xl mb-8">Login form</h3>
+       <Field type="text"></Field>
+       <button class="inline-flex items-center justify-center font-bold border shadow-xl rounded cursor-pointer min-h-[42px] py-4 px-12 bg-purple-500 text-white hover:bg-purple-700 mx-auto my-3 min-w-[400px]">Submit</button>
+     </Form>
+
    </div>
 </template>
 
