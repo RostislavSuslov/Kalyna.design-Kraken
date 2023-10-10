10a7116eaa91ef4325b923cd3b56e2c951cadedd
RostSuslov <suslovros@gmail.com>
2023-10-10 15:37:37 +0300

    build: install validate libary yup

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,12 @@
-<script setup>
-// import { provide, reactive } from 'vue';
-// import arrCities from "@/composables/cities.js";
-// import { useVuelidate } from '@vuelidate/core';
-// import { required, email } from '@vuelidate/validators';
-
-import FormLogin from "@/components/provide/FormLogin.vue";
-
-// const input = ref('')
-// const inputError = computed(()=>{
-//     return input.value.trim().length ? '' : 'Input is required'
-// })
-//
-// const helperText = computed(()=>{
-//   return "A smile always expresses positive emotions: happiness, joy, fun. Smile at someone and they'll smile back :)"
-// });
-//provide('cities', arrCities);
-
-// const state = reactive({
-//   firstName: 'Rost',
-//   lastName: '',
-//   contact: {
-//     email: ''
-//   }
-// });
-//
-// const rules = {
-//   firstName: { required }, // Matches state.firstName
-//   lastName: { required }, // Matches state.lastName
-//   contact: {
-//     email: { required, email } // Matches state.contact.email
-//   }
-// };
-//
-// const v$ = useVuelidate(rules, state);
-</script>
-
-
 <template>
-   <div id="root" class="container ">
-     <FormLogin/>
-<!--     <form @submit="sendForm" name="field" class="flex flex-col items-center border p-12">-->
-<!--       <h3 class="text-4xl mb-8">Login form</h3>-->
-<!--       <label for="name"></label>-->
-<!--       <input id="name" class="border p-5 w-full" placeholder="write name"/>-->
-<!--       <button class="inline-flex items-center justify-center font-bold border shadow-xl rounded cursor-pointer min-h-[42px] py-4 px-12 bg-purple-500 text-white hover:bg-purple-700 mx-auto my-3 min-w-[400px]">Submit</button>-->
-<!--     </form>-->
-<!--     <form :class="{ error: v$.firstName.$errors.length }">-->
-<!--       <input v-model="state.firstName" class="border p-5 w-full" placeholder="write name"/>-->
-<!--       <div class="input-errors" v-for="error of v$.firstName.$errors" :key="error.$uid">-->
-<!--         <div class="error-msg">{{ error.$message }}</div>-->
-<!--       </div>-->
-<!--       <button>submit</button>-->
-<!--     </form>-->
-   </div>
+  <div class="container ">
+    <login-form></login-form>
+  </div>
 </template>
 
-<style lang="scss">
-  @import './assets/css/style.scss';
-</style>
+<script setup>
+import LoginForm from "@/components/provide/LoginForm.vue";
+import {ref} from "vue";
+
+const input = ref('')
+</script>
--- src/components/provide/CustomInput.vue
+++ src/components/provide/CustomInput.vue
@@ -1,70 +1,101 @@
+<template>
+  <wrapper-custom-input :helperText="helperText" :error="error" :label="label">
+    <input
+        :type="type"
+        :placeholder="placeholder"
+        class="w-full px-4 py-2 rounded-md border-transparent placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        @input="onSetValue"
+        :value="value"
+    />
+  </wrapper-custom-input>
+</template>
+
 <script setup>
+
 import WrapperCustomInput from "@/components/provide/WrapperCustomInput.vue";
-import {computed, inject} from "vue";
-import {useField} from "@/composables/form"
+import { useField } from "@/composables/form.js";
 
-const form = inject('formData');
-
-defineEmits(
-    ['update:modelValue', ]
-)
+defineEmits(['update:modelValue'])
 
 const props = defineProps({
   placeholder: String,
   label: String,
-  error: String,
   helperText: String,
   modelValue: String,
-  type:  {
+  type: {
     type: String,
-    default: "text"
+    default: "text",
   },
   name: String,
 })
-const innerValue = computed({
-  get() {
-    return form.value[props.name]
-  },
-  set(val) {
-    form.value[props.name] = val;
-  }
-})
+
+const {value, error, onSetValue} = useField(props);
+
 </script>
-<template>
-  <wrapper-custom-input :helperText="helperText" :error="error" :label="label">
-    <input :type="type"
-           :placeholder="placeholder"
-           class="peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-           v-model="innerValue"
-    >
-  </wrapper-custom-input>
-</template>
 
-<style scoped lang="scss"></style>
+//old version
+<!--<template>-->
+<!--  <wrapper-custom-input :helperText="helperText" :error="error" :label="label">-->
+<!--    <input-->
+<!--      :type="type"-->
+<!--      :placeholder="placeholder"-->
+<!--      class="peer block min-h-[auto] w-full rounded ring-1 bg-transparent px-3 py-[0.32rem]"-->
+<!--      v-model="modelValue"-->
+<!--    />-->
+<!--  </wrapper-custom-input>-->
+<!--</template>-->
 
+<!--<script setup>-->
+<!--import WrapperCustomInput from "@/components/privide-inject/WrapperCustomInput.vue";-->
+<!--import { computed, watch, inject } from "vue";-->
 
+<!--const props = defineProps({-->
+<!--  placeholder: String,-->
+<!--  label: String,-->
+<!--  error: String,-->
+<!--  helperText: String,-->
+<!--  modelValue: String,-->
+<!--  type: {-->
+<!--    type: String,-->
+<!--    default: "text",-->
+<!--  },-->
+<!--  name: String,-->
+<!--});-->
 
+<!--const formData = inject("formData");-->
+<!--const formErrors = inject("formErrors");-->
 
+<!--const validationRules = {-->
+<!--  login: {-->
+<!--    required: "Login is required",-->
+<!--  },-->
+<!--  password: {-->
+<!--    required: "Password is required",-->
+<!--  },-->
+<!--};-->
 
+<!--const modelValue = computed({-->
+<!--  get() {-->
+<!--    return formData.value[props.name];-->
+<!--  },-->
+<!--  set(val) {-->
+<!--    formData.value[props.name] = val;-->
+<!--    validateField(props.name);-->
+<!--  },-->
+<!--});-->
 
+<!--const validateField = (fieldName) => {-->
+<!--  if (!modelValue.value && validationRules[fieldName]?.required) {-->
+<!--    formErrors.value[fieldName] = validationRules[fieldName].required;-->
+<!--  } else {-->
+<!--    formErrors.value[fieldName] = "";-->
+<!--  }-->
+<!--};-->
 
-
-
-
-
-
-
-
-<!--<div class="relative mb-3" data-te-input-wrapper-init>-->
-<!--<input-->
-<!--    type="text"-->
-<!--    class="peer block min-h-[auto] w-full rounded   ring-1 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"-->
-<!--    id="exampleFormControlInput1"-->
-<!--    placeholder="Example label"-->
-<!--/>-->
-<!--<label-->
-<!--    for="exampleFormControlInput1"-->
-<!--    class="pointer-events-none bg-white absolute left-3 top-0 mb-0  origin-[0_0] truncate p-[0.37rem] leading-[1] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.8rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.8rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"-->
-<!--&gt;Example label-->
-<!--</label>-->
-<!--</div>-->
+<!--watch(-->
+<!--  () => formData.value[props.name],-->
+<!--  () => {-->
+<!--    validateField(props.name);-->
+<!--  },-->
+<!--);-->
+<!--</script>-->
